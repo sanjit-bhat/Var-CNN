418f1a45def4749f456f38eed62ae207befeafd2
sanjit-bhat <sanjit.bhat@gmail.com>
2018-02-11 11:19:08 -0500

    Update formatting
    Update out file, num trials, and line spacing.

--- run_open_closed_world.py
+++ run_open_closed_world.py
@@ -8,19 +8,20 @@ import run_ensemble
 import evaluate_ensemble_confidence
 
 orig_stdout = sys.stdout
-f = open('results.txt', 'a', 1)
+f = open('run_open_closed_world_out.txt', 'a', 1)
 sys.stdout = f
 
 # closed-world
-for i in range(1, 1):
+for i in range(1, 11):
     print("closed-world try = ", i)
-    # calls are in the format (num_sens_sites, num_sens_inst_test, num_sens_inst_train, num_insens_sites_test, num_insens_sites_train)
+    # calls are in the format (num_sens_sites, num_sens_inst_test, num_sens_inst_train,
+    # num_insens_sites_test, num_insens_sites_train)
     preprocess_script.main(100, 30, 60, 0, 0)
     run_ensemble.main(100, 30, 60, 0, 0)
     evaluate_ensemble_confidence.main(100, 30, 60, 0, 0)
 
 # open-world
-for i in range(6, 11):
+for i in range(1, 11):
     print("open-world try = ", i)
     preprocess_script.main(100, 30, 60, 5500, 3500)
     run_ensemble.main(100, 30, 60, 5500, 3500)
