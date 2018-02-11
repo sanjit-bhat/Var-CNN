2d634950e3ea3f60c730923914fc0175bfbd77da
Sanjit Bhat <sanjit.bhat@gmail.com>
2018-02-11 12:47:19 -0500

    Update file imports

--- run_open_closed_world.py
+++ run_open_closed_world.py
@@ -3,9 +3,9 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
-import preprocess_script
-import run_ensemble
-import evaluate_ensemble_confidence
+import preprocess_data
+import var_cnn_ensemble
+import evaluate_ensemble
 
 orig_stdout = sys.stdout
 f = open('run_open_closed_world_out.txt', 'a', 1)
@@ -16,16 +16,16 @@ for i in range(1, 11):
     print("closed-world try = ", i)
     # calls are in the format (num_sens_sites, num_sens_inst_test, num_sens_inst_train,
     # num_insens_sites_test, num_insens_sites_train)
-    preprocess_script.main(100, 30, 60, 0, 0)
-    run_ensemble.main(100, 30, 60, 0, 0)
-    evaluate_ensemble_confidence.main(100, 30, 60, 0, 0)
+    preprocess_data.main(100, 30, 60, 0, 0)
+    var_cnn_ensemble.main(100, 30, 60, 0, 0)
+    evaluate_ensemble.main(100, 30, 60, 0, 0)
 
 # open-world
 for i in range(1, 11):
     print("open-world try = ", i)
     preprocess_script.main(100, 30, 60, 5500, 3500)
-    run_ensemble.main(100, 30, 60, 5500, 3500)
-    evaluate_ensemble_confidence.main(100, 30, 60, 5500, 3500)
+    var_cnn_ensemble.main(100, 30, 60, 5500, 3500)
+    evaluate_ensemble.main(100, 30, 60, 5500, 3500)
     
 sys.stdout = orig_stdout
 f.close()
