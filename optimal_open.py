f2926950b8561e7879c79a8244c7225be87bf086
Sanjit Bhat <sanjit.bhat@gmail.com>
2018-02-27 11:33:18 -0500

    Add future

--- optimal_open.py
+++ optimal_open.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 # Optimal attacker TPR, FPR, precision, F1 score -- open-world
 
 import math
